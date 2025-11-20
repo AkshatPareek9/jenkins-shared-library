170b6665e38d4b09037c9c1af3de5741da9f4b6f
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-21 03:33:27 +0530

    Update docker_push.groovy

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -5,7 +5,8 @@ def call(String imagename, String imageversion) {
         'passwordVariable': "dockerhubpass", 
         'usernameVariable': "dockerhubuser"
     )]){
-    sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
-    sh "docker image tag ${imagename}:${imageversion} ${dockerhubuser}/${imagename}:${imageversion}"
-    sh "docker push ${dockerhubuser}/${imagename}:${imageversion}"    
+        sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+        sh "docker image tag ${imagename}:${imageversion} ${dockerhubuser}/${imagename}:${imageversion}"
+        sh "docker push ${dockerhubuser}/${imagename}:${imageversion}"    
+    }
 }
