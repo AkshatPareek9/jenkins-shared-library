f1b69074275a2e6fef8b2b0f7f6e617b092e5fbe
Akshat Pareek <62641746+AkshatPareek9@users.noreply.github.com>
2025-11-21 03:21:15 +0530

    Create docker-push.groovy

---
+++ vars/docker-push.groovy
@@ -0,0 +1,11 @@
+def call(String imagename, String imageversion) {
+    echo "Pushing the image to DockerHub"
+    withCredentials([usernamePassword(
+        'credentialsId': "dockerhubcred", 
+        'passwordVariable': "dockerhubpass", 
+        'usernameVariable': "dockerhubuser"
+    )]){
+    sh "docker login -u ${dockerhubuser} -p ${dockerhubpass}"
+    sh "docker image tag ${imagename}:${imageversion} ${dockerhubuser}/${imagename}:${imageversion}"
+    sh "docker push ${dockerhubuser}/${imagename}:${imageversion}"    
+}
